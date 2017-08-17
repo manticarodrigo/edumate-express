509dbc90dbc1d62b77782bf499ef120fc097c2af
Rodrigo Mantica <manticarodrigo@gmail.com>
2017-08-17 12:23:20 -0600

    img path changed

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -7,7 +7,6 @@ exports.search = function(req, res, next) {
   var savedData = [];
   
   request(searchUrl, function(err, response, html) {
-    // console.log(err || html); // Print out the HTML
     // First we'll check to make sure no errors occurred when making the request
     if (err) {
       return res.status(500).send(err);
@@ -15,11 +14,11 @@ exports.search = function(req, res, next) {
     var $ = cheerio.load(html);
     $('div.g').each(function(i, element) {
       var title = $(this).find('.r').text();
-      var link = $(this).find('.r').find('a').attr('href');
+      var link = $(this).find('.r').find('a').attr('href').replace('/url?q=', '').split('&')[0];
       var text = $(this).find('.st').text();
       var img = $(this).find('img.th').attr('src');
       savedData.push({
-        imageUrl: 'assets/edumate.png',
+        imageUrl: 'assets/images/edumate.png',
         firstName: 'Edumate',
         lastName: 'Bot',
         text: 'Based on your interest in ' + searchTerm + '...',
