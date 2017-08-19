a6ba98142547afb0cabf1e374a1f41a29f6f72ee
Rodrigo Mantica <manticarodrigo@gmail.com>
2017-08-18 19:37:49 -0600

    interest controller

--- app/controllers/interest.js
+++ app/controllers/interest.js
@@ -1,39 +1,39 @@
 const Interest = require('../models/interest');
 
 exports.getInterests = function(req, res, next) {
- Interest.find({
-   user_id: req.params.user_id
+  Interest.find({
+    user_id: req.params.user_id
   }, function(err, interests) {
-   if (err) {
-     return res.send(err);
-   }
-   res.json(interests);
- });
+    if (err) {
+      return res.send(err);
+    }
+    res.json(interests);
+  });
 }
 
 exports.createInterest = function(req, res, next) {
- Interest.create(req.body, function(err, interest) {
-   if (err) {
-     return res.status(500).send(err);
-   }
-   Interest.find({
+  Interest.create(req.body, function(err, interest) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    Interest.find({
     user_id: req.params.user_id
-   }, function(err, interests) {
-     if (err) {
-       return res.status(500).send(err);
-     }
-     res.json(interests);
-   });
- });
+    }, function(err, interests) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      res.json(interests);
+    });
+  });
 }
 
 exports.deleteInterest = function(req, res, next) {
- Interest.remove({
-   _id : req.params.interest_id
- }, function(err, interest) {
-   if (err) {
-     return res.status(500).send(err);
-   }
-   res.json(interest);
- });
+  Interest.remove({
+    _id : req.params.interest_id
+  }, function(err, interest) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(interest);
+  });
 }
